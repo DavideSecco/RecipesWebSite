6994387cbdf910e77a677c6cd56bf7c86f3c4006
davide <d.secco2000@gmail.com>
2022-09-19 12:57:13 +0200

    creata la home e il primo modello

--- recipes/gestione/models.py
+++ recipes/gestione/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Ricetta(models.Model):
+    nome = models.CharField(max_length=50)
+    tempo = models.TimeField
+    difficoltá = models.CharField(max_length=50)
+
+    def __str__(self):
+        out = self.nome
