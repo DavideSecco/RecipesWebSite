6e2fcaf12dbae6f5a017c76452acf6e3033f4930
davide <d.secco2000@gmail.com>
2022-10-19 18:31:22 +0200

    Ultimi fix

--- recipes/gestione/tests.py
+++ recipes/gestione/tests.py
@@ -10,7 +10,6 @@ from django.test.utils import setup_test_environment
 # Create your tests here.
 
 
-
 class ListViewTests(TestCase):
     def test_lista_ricette_with_no_ricette(self):
         """
@@ -50,9 +49,8 @@ class ListViewTests(TestCase):
         self.assertQuerysetEqual(response.context["object_list"], [r1])
 
 
-
 class RicettaDetailViewTests(TestCase):
-    def test_funzioni_per_persone_loggate_non_sono_attive_quando_utente_non_e_loggato(self):
+    def test_funzioni_per_persone_loggate_non_attive_quando_utente_non_loggato(self):
         r1 = Ricetta.objects.create(nome='ricetta 1')
 
         response = self.client.get(reverse("ricetta", kwargs={"ricetta_id": r1.id}))
@@ -61,8 +59,7 @@ class RicettaDetailViewTests(TestCase):
         self.assertNotContains(response, "Aggiungi ai preferiti")
         self.assertNotContains(response, "Valuta")
 
-
-    def test_funzioni_per_persone_loggate_sono_attive_quando_utente_e_loggato(self):
+    def test_funzioni_per_persone_loggate_attive_quando_utente_e_loggato(self):
         # Creo utente e ricetta dell'utente
         self.user = User.objects.create_user(username='davide', email='davide@prova.it', password='davide')
         r1 = Ricetta.objects.create(nome='ricetta 1', utente=self.user)
@@ -76,3 +73,32 @@ class RicettaDetailViewTests(TestCase):
         self.assertContains(response, "Aggiungi ai preferiti")
         self.assertContains(response, "Valuta")
 
+    def test_modifica_e_cancellazione_se_utente_e_proprietario_ricetta(self):
+        # Creo utente e ricetta dell'utente
+        self.user = User.objects.create_user(username='davide', email='davide@prova.it', password='davide')
+        r1 = Ricetta.objects.create(nome='ricetta 1', utente=self.user)
+
+        # eseguo il login #
+        self.client.login(username="davide", password="davide")
+
+        # tests #
+        response = self.client.get(reverse("ricetta", kwargs={"ricetta_id": r1.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Modifica Ricetta")
+        self.assertContains(response, "Cancella Ricetta")
+
+
+    def test_modifica_e_cancellazione_se_utente_non_e_proprietario_ricetta(self):
+        # Creo utenti e ricetta dell'utente
+        self.user = User.objects.create_user(username='davide', email='davide@prova.it', password='davide')
+        self.user_no_proprietario = User.objects.create_user(username='d', email='d@prova.it', password='d')
+        r1 = Ricetta.objects.create(nome='ricetta 1', utente=self.user)
+
+        # eseguo il login #
+        self.client.login(username="d", password="d")
+
+        # tests #
+        response = self.client.get(reverse("ricetta", kwargs={"ricetta_id": r1.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "Modifica Ricetta")
+        self.assertNotContains(response, "Cancella Ricetta")
